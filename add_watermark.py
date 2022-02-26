d6c172473d2f7df966f590d6ad79dc1bf66f18fa
txtxj <1418373194@qq.com>
2022-02-26 15:30:04 +0800

    feat: add spectrum watermark

---
+++ add_watermark.py
@@ -0,0 +1,19 @@
+import cv2
+from matplotlib import pyplot as plt
+from watermark import add_watermark
+
+img = cv2.imread("./pictures/avatar.jpg", 1)
+watermark = cv2.imread("./pictures/watermark.jpg", 0)
+
+img_watermark = add_watermark(img, watermark, 20000)
+
+# print image
+plt.subplot(1, 2, 1), plt.imshow(img[:, :, [2, 1, 0]])
+plt.title("input image"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(1, 2, 2), plt.imshow(img_watermark[:, :, [2, 1, 0]])
+plt.title("watermark"), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+cv2.imwrite("./pictures/output.jpg", img_watermark)
---
+++ watermark.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+
+def decrypt_single(img):
+    f = np.fft.fft2(img)
+    f_shift = np.fft.fftshift(f)
+    spectrum = np.log(np.abs(f_shift))
+    maxx = np.max(spectrum)
+    minn = np.min(spectrum)
+    spectrum = (spectrum - minn) / (maxx - minn) * 255
+    return spectrum
+
+
+def decrypt(img):
+    img_b, img_g, img_r = cv2.split(img)
+    spectrum_b = decrypt_single(img_b)
+    spectrum_g = decrypt_single(img_g)
+    spectrum_r = decrypt_single(img_r)
+    return spectrum_b, spectrum_g, spectrum_r
+
+
+def add_watermark_single(img, watermark, coefficient):
+    h, w = watermark.shape
+
+    f = np.fft.fft2(img)
+    f_shift = np.fft.fftshift(f)
+    f_shift[0:h, 0:w] += watermark * coefficient
+    f_shift[-h:, -w:] += cv2.flip(watermark, -1) * coefficient
+
+    if_shift = np.fft.ifftshift(f_shift)
+    img_watermark = np.fft.ifft2(if_shift)
+    img_watermark = np.abs(img_watermark)
+    img_watermark = np.uint8(img_watermark)
+    return img_watermark
+
+
+def add_watermark(img, watermark, coefficient):
+    img_b, img_g, img_r = cv2.split(img)
+    img_h, img_w = img.shape[0], img.shape[1]
+    wm_h, wm_w = watermark.shape[0], watermark.shape[1]
+    ratio_h = img_h / wm_h / 2
+    ratio_w = img_w / wm_w / 2
+    ratio = ratio_h if ratio_w > ratio_h else ratio_w
+    wm_h = np.int(wm_h * ratio)
+    wm_w = np.int(wm_w * ratio)
+
+    watermark = cv2.resize(watermark, (wm_w, wm_h))
+    watermark = 255 - watermark
+    img_b_watermark = add_watermark_single(img_b, watermark, coefficient)
+    img_g_watermark = add_watermark_single(img_g, watermark, coefficient)
+    img_r_watermark = add_watermark_single(img_r, watermark, coefficient)
+
+    img_watermark = cv2.merge([img_b_watermark, img_g_watermark, img_r_watermark])
+    return img_watermark
